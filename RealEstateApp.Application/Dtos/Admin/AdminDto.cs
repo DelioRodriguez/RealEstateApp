8754c3665b9abafc66d82dbfb901a00b9ff4fe7f
Dayner-Garcia <dayner1227garcia@gmail.com>
2024-12-05 19:29:55 -0400

    .

--- RealEstateApp.Application/Dtos/Admin/AdminDto.cs
+++ RealEstateApp.Application/Dtos/Admin/AdminDto.cs
@@ -1,13 +1,20 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace RealEstateApp.Application.Dtos.Admin;
 
 public class AdminDto
 {
     public string? Id { get; set; }
+    [Required(ErrorMessage = "El username es requerido")]
     public string? UserName { get; set; }
+    [Required(ErrorMessage = "El email es requerido")]
     public string? Email { get; set; }
+    [Required(ErrorMessage = "El Nombre es requerido")]
     public string FirstName { get; set; }
+    [Required(ErrorMessage = "El Apellido es requerido")]
     public string LastName { get; set; }
     public string? Password { get; set; }
+    [Compare("Password", ErrorMessage = "Las contraseñas no coinciden.")]
     public string? ConfirmPassword { get; set; }
     public bool IsActive { get; set; }
 }
--- RealEstateApp.Application/Dtos/Developer/DeveloperDto.cs
+++ RealEstateApp.Application/Dtos/Developer/DeveloperDto.cs
@@ -1,13 +1,19 @@
 namespace RealEstateApp.Application.Dtos.Developer;
+using System.ComponentModel.DataAnnotations;
 
 public class DeveloperDto
 {
     public string? Id { get; set; }
+    [Required(ErrorMessage = "El username es requerido")]
     public string? UserName { get; set; }
+    [Required(ErrorMessage = "El email es requerido")]
     public string? Email { get; set; }
+    [Required(ErrorMessage = "El Nombre es requerido")]
     public string FirstName { get; set; }
+    [Required(ErrorMessage = "El Apellido es requerido")]
     public string LastName { get; set; }
     public string? Password { get; set; }
+    [Compare("Password", ErrorMessage = "Las contraseñas no coinciden.")]
     public string? ConfirmPassword { get; set; }
     public bool IsActive { get; set; } 
 }
